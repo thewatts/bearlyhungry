d8019c57a5a64cb9d865b68ea75ae964a6572178
Antony Siegert <agsiegert@hotmail.com>
2013-11-04 17:05:13 -0700

    initial comit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wtpho::Application.config.secret_key_base = 'b58f5b18b5cc6e3a909caa461fef8257fa73387f24b47a1c26ec728cffff4746e7bdc5006535432362900e013bf004fd95b8d4a27baf7acf5d1190336e82728f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Wtpho::Application.config.session_store :cookie_store, key: '_wtpho_session'
