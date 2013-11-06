1ef8513912c2576221022235fd3cd60ec56d36f3
Antony Siegert <agsiegert@hotmail.com>
2013-11-06 09:48:57 -0700

    added columns to users with db:migrate

---
+++ db/migrate/20131106164405_add_column_to_users.rb
@@ -0,0 +1,6 @@
+class AddColumnToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :full_name, :string
+    add_column :users, :display_name, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131105234423) do
+ActiveRecord::Schema.define(version: 20131106164405) do
 
   create_table "items", force: true do |t|
     t.string   "title"
@@ -28,6 +28,8 @@ ActiveRecord::Schema.define(version: 20131105234423) do
   create_table "users", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "full_name"
+    t.string   "display_name"
   end
 
 end
