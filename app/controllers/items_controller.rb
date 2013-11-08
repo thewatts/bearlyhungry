a65d6e1e3e23b8a37dad0a292d0a578f8fa45de2
Antony Siegert <agsiegert@hotmail.com>
2013-11-07 22:28:32 -0700

    reformatted views, added edit, show, delete, update item_helper which eliminated item_attribute in item_controller. Able to click on item title, upload an image and it shows in Item show page.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,15 @@
 class ItemsController < ApplicationController
+  include ItemsHelper
   include CurrentOrder
+
+
   def index
     @current_order
     @items = Item.all
+  end
+
+  def show
+    @item = Item.find(params[:id])
   end
 
   def new
@@ -10,16 +17,30 @@ class ItemsController < ApplicationController
   end
 
   def create
-    item = Item.new(item_attributes)
+    item = Item.new(item_params)
     item.save
+    redirect_to items_path(@item)
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def destroy
+    @item = Item.find(params[:id])
+    item.destroy
     redirect_to items_path
   end
 
-
-  def item_attributes
-    params.require(:item)
-          .permit(:title, :description, :price)
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    redirect_to items_path(@item)
   end
-
-
 end
+
+
+  # def item_attributes
+  #   params.require(:item)
+  #         .permit(:title, :description, :price)
+  # end
--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,2 +1,5 @@
 module ItemsHelper
+  def item_params
+    params.require(:item).permit(:title, :description, :price, :image)
+  end
 end
