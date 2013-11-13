d38b12f8f889e09b5d2703bfe98d426c84909d4b
Ben Horne <benhorne44@gmail.com>
2013-11-13 12:19:51 -0700

    Add partials for views while setting up order for stripe

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -9,7 +9,7 @@ class OrdersController < ApplicationController
   end
 
   def show
-    @order = Order.find(set_order.id)
+    @order = Order.find(params[:id])
     # fail
     if current_user
       @order.user_id = current_user.id
