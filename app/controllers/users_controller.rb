6ddd0a7dac5d1d099ff506d6df46cf2d22a52332
Antony Siegert <agsiegert@hotmail.com>
2013-11-13 15:46:13 -0700

    repairs to action mailer in user controller, config/environments/development.rb uses mailcatcher and config/environments/production.rb uses gmail

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,13 +16,9 @@ class UsersController < ApplicationController
   def create
     user = User.new(user_params)
     if user.save
-      current_order.update(user_id: user.id, status: "in_progress")
+      set_order.update(user_id: user.id, status: "in_progress")
       session[:user_id] = user.id
-      redirect_to :back
-      UserMailer.welcome_email(@user).deliver
-
-        format.html { redirect_to(@user, notice: 'User was successfully created.') }
-
+      UserMailer.welcome_email(user).deliver
      redirect_to user_path(user)
     else
       # user.errors.each do |error|
