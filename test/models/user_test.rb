90d1f05cd1904ef23c7cc93f5f32e4d7bca14c1c
Nathaniel Watts <reg@nathanielwatts.com>
2013-11-14 08:00:36 -0700

    refactor initial user tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,30 +2,34 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
 
-    test "user must exist" do
-      user = FactoryGirl.build(:user, full_name: "")
-      refute user.save
+    test "user is saved with correct attributes " do
+      user = FactoryGirl.build(:user, full_name: "Antony THEMAN",
+                                      email: "asdf@example.com",
+                                      password: "password",
+                                      password_confirmation: "password",
+                                      display_name: "A DAWG")
+      assert user.save
     end
 
     test "user must have full name which is not blank" do
       user1 = FactoryGirl.build(:user, full_name: "")
-      user2 = FactoryGirl.build(:user, full_name: "Jerome Bartholomew")
 
       refute user1.save
-      assert user2.save
+    end
 
+    test "user may have a blank display name" do
+      user = FactoryGirl.build(:user, display_name: "")
+      assert user.save
     end
 
     test "user may have display name 32 > size > 2" do
-      user1 = FactoryGirl.build(:user, display_name: "")
-      user2 = FactoryGirl.build(:user, display_name: "A")
-      user3 = FactoryGirl.build(:user, display_name: "asdfghjklqwertyuiopzxcvbnmasdfghj")
-      user4 = FactoryGirl.build(:user, display_name: "Squeaky")
+      user1 = FactoryGirl.build(:user, display_name: "A")
+      user2 = FactoryGirl.build(:user, display_name: "asdfghjklqwertyuiopzxcvbnmasdfghj")
+      user3 = FactoryGirl.build(:user, display_name: "Squeaky")
 
-      assert user1.save #should still save if no display_name
+      refute user1.save
       refute user2.save
-      refute user3.save
-      assert user4.save
+      assert user3.save
     end
 
 end
