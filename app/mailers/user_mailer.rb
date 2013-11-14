56a4dfb18e34fbe11ae5272cc8146db70f900209
Nathaniel Watts <reg@nathanielwatts.com>
2013-11-13 22:30:02 -0700

    add domain

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,7 +4,7 @@ class UserMailer < ActionMailer::Base
   def welcome_email(user)
     @user = user
     @url = 'http://wtpho.herokuapp.com/login'
-    mail( {to: @user.email,
+    mail({to: @user.email,
           from: 'wtphok@gmail.com',
           subject: 'Welcome to What the Pho'})
   end
