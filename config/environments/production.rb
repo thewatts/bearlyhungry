930cbdcca47091fad7923717aa151b54aca96995
Nathaniel Watts <reg@nathanielwatts.com>
2013-11-13 22:31:01 -0700

    add comma

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,7 @@ Wtpho::Application.configure do
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    domain:               'gmail.com'
+    domain:               'gmail.com',
     user_name:            'wtphok',
     password:             'Antony12',
     authentication:       'plain',
