47f7d5b0d71e5e322270911d8b8cbba623bdc000
Nathaniel Watts <reg@nathanielwatts.com>
2013-11-13 21:51:43 -0700

    rename Action_mailer to ActionMailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Wtpho::Application.configure do
-  Action_mailer.delivery_method = :smtp
-  Action_mailer.smtp_settings = {
+  ActionMailer.delivery_method = :smtp
+  ActionMailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
     domain:               'wtpho.herokuapp.com',
