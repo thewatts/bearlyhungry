240da28885ada0452ab923f6a0eea9b6d572e5c1
Nathaniel Watts <reg@nathanielwatts.com>
2013-11-13 22:21:44 -0700

    change ActionMailer to config.action_mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Wtpho::Application.configure do
-  ActionMailer::Base.delivery_method = :smtp
-  ActionMailer::Base.smtp_settings = {
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
     user_name:            'wtphok',
