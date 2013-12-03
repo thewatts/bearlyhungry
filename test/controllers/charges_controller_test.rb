d449a194dda0cc0f4b319b58f14f2437978bd63c
Katrina Engelsted <katrina@engelsted.co>
2013-12-03 15:49:00 -0700

    editing charge controllers

--- test/controllers/charges_controller_test.rb
+++ test/controllers/charges_controller_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class ChargesControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  test "doesn't charge card unless logged in" do
+    
+  end
+
+  test "doesn't charge card unless something is in cart" do
+
+  end
+
+  test ""
+
 end
