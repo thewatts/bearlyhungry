300f6eefe5c689a78f2e120e074d2d4385984405
Katrina Engelsted <katrina@engelsted.co>
2013-12-05 12:29:03 -0700

    adding charges controller edits and features test on user checkout

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -3,11 +3,11 @@ class ChargesController < ApplicationController
   before_action :validate_order
 
   def new
-    @order_number = set_order.id
+    @order_number = current_order.id
+    @amount = current_order.subtotal
   end
 
   def create
-    @amount = (set_order.subtotal * 100).to_i
     customer = Stripe::Customer.create(
       :email => current_user.email,
       :card  => params[:stripeToken]
@@ -15,14 +15,14 @@ class ChargesController < ApplicationController
 
     charge = Stripe::Charge.create(
       :customer    => customer.id,
-      :amount      => @amount,
+      :amount      => (current_order.subtotal * 100).to_i,
       :description => 'BearyHungry customer',
       :currency    => 'usd'
     )
 
-    flash[:successful_transaction] = "Thank you! Your order number is '#{set_order.id}.' A receipt was sent to #{customer.email}. We'll email you when your order is completed."
+    flash[:successful_transaction] = "Thank you! Your order number is '#{current_order.id}.' A receipt was sent to #{customer.email}. We'll email you when your order is completed."
    
-    @current_order.update(status: "paid")
+    current_order.update(status: "paid")
     session[:order_id] = nil
     redirect_to menu_path
 
@@ -34,12 +34,12 @@ class ChargesController < ApplicationController
   private
 
   def validate_order
-    @amount = set_order.subtotal
-    if @amount == 0
+    if current_order.order_items.count == 0
       flash[:error] = ["Please add items to your order before proceeding."]
-    elsif session[:user_id].nil?
+      redirect_to menu_path
+    elsif current_user.nil?
       flash[:error] = ["You must login or sign up before paying."]
+      redirect_to menu_path
     end
-    redirect_to menu_path
   end
 end
---
+++ spec/controllers/charges_controller_spec.rb
@@ -0,0 +1,32 @@
+# require_relative '../spec_helper'
+
+# describe ChargesController do
+  
+#   before do
+#     @order = Order.create(status: "pending")
+#     @user = User.create(
+#       email: "test@example.com",
+#       password: "password",
+#       password_confirmation: "password",
+#       full_name: "Example User"
+#     ) 
+#     #factory girl?
+#   end
+
+#   describe "#new" do
+#     context "when charged" do
+#       expect(response.status).to eq(200)
+#       redirect_to menu_path
+#     end
+#   end
+
+#   describe "#create" do
+#     context "creates a new charge to user" do
+#   #   post(:create, nil, { order_id: @order.id, user_id: @user.id })
+#   #   assert_response 302
+#   #   assert_redirected_to menu_path
+#   #   # assert_equal "", flash[:successful_transaction]
+#   #   #controller: "items", action: "index" --> more direct way to redirect test
+#     end
+#   end
+# end
