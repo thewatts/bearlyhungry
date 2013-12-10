0ba7e832297743831cbcaf8646610e106ced10a4
Nathaniel Watts <reg@nathanielwatts.com>
2013-12-10 00:03:23 -0700

    add pickup sms

--- app/models/user.rb
+++ app/models/user.rb
@@ -35,4 +35,13 @@ class User < UserBase
 
     SMS.send_message(number_to_send_to, body)
   end
+
+  def pickup_sms_for(order)
+    number_to_send_to = phone_number.to_i
+    item_count = order.items.count
+    body = "Success, #{full_name}!\n"
+    body << "Your order is ready for pickup!"
+
+    SMS.send_message(number_to_send_to, body)
+  end
 end
