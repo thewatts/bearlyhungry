73de11b1e7e59045184a4be22e254a562cc6e48a
Nathaniel Watts <reg@nathanielwatts.com>
2013-12-10 00:07:34 -0700

    move user methods into user_base, as they are needed for guest as well

--- app/models/user_base.rb
+++ app/models/user_base.rb
@@ -10,5 +10,23 @@ class UserBase < ActiveRecord::Base
     orders.last
   end
 
+  def confirmation_sms_for(order)
+    number_to_send_to = phone_number.to_i
+    item_count = order.items.count
+    body = "Thanks, #{full_name}, for your order of #{item_count} items!"
+    body << "We'll send you another text when it's ready for pickup!"
+
+    SMS.send_message(number_to_send_to, body)
+  end
+
+  def pickup_sms_for(order)
+    number_to_send_to = phone_number.to_i
+    item_count = order.items.count
+    body = "Success, #{full_name}!\n"
+    body << "Your order is ready for pickup!"
+
+    SMS.send_message(number_to_send_to, body)
+  end
+
 end
 
