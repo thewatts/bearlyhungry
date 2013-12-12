d3faa72b9d5c24159da26e95346f424086a75543
Nathaniel Watts <reg@nathanielwatts.com>
2013-12-11 20:46:53 -0700

    add Restaurant Model

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,7 @@
+class RestaurantsController < ApplicationController
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131207223112) do
+ActiveRecord::Schema.define(version: 20131211225500) do
 
   create_table "categories", force: true do |t|
     t.datetime "created_at"
@@ -63,6 +63,12 @@ ActiveRecord::Schema.define(version: 20131207223112) do
   end
 
   add_index "orders", ["user_id"], name: "index_orders_on_user_id"
+
+  create_table "restaurants", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.datetime "created_at"
