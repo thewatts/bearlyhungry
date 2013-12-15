9919acfe4c7b3b6249901c5bb8963bd4d483afad
Nathaniel Watts <reg@nathanielwatts.com>
2013-12-15 01:08:18 -0700

    move restaurant into restaurants and add intiial orders spec

---
+++ spec/features/orders/user_creates_multiple_orders_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+feature "User Order Creation" do
+
+  scenario "creates restaurant from new restaurant page" do
+    #As an authenticated user
+    sign_in(user)
+    #when I visit the new restaurant page
+    visit new_restaurant_path
+    #and I click "Create A New Restaurant"
+    #and I fill in the name
+    fill_in "Your Restaurant Name", with: "Test Restaurant"
+    fill_in "The URL of your restaurant", with: "test"
+    #and I click "Create Restaurant"
+    click_on "Start My Restaurant Now"
+    expect(page.current_path).to eq admin_restaurant_path("test")
+    #then the platform admin receives an email notification
+    #and I receive an email notification
+    #and the new restaurant is listed as pending in the admin interface
+    expect(page).to have_content "Pending"
+    #and the restaurant is not visible to customers
+    visit restaurant_root_path("test")
+    expect(page).to have_content "Sorry,"
+  end
+end
