9298e9cb76a1545fd4bc0c7a6c36fc213f324654
Nathaniel Watts <reg@nathanielwatts.com>
2013-12-17 16:31:59 -0700

    update seeds until dup orders

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ActiveRecord::Base
 
   validates :title, :description, :price, presence: true
-  validates :title, uniqueness: true
+  #validates :title, uniqueness: true
   validates :price, numericality: { greater_than: 0 }
 
   has_attached_file :image
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -11,7 +11,7 @@ class Restaurant < ActiveRecord::Base
 
   def self.create_with_owner(restaurant_params, user)
     restaurant = Restaurant.create!(restaurant_params)
-    Job.create!(:restaurant => restaurant, :user => user, :role => Role.owner)
+w   Job.create!(:restaurant => restaurant, :user => user, :role => Role.owner)
     restaurant.send_new_restaurant_confirmation_email(user)
     restaurant.send_new_restaurant_submitted_email
     restaurant
@@ -19,6 +19,14 @@ class Restaurant < ActiveRecord::Base
 
   def add_owner(user)
     Job.create!(:restaurant => self, :user => user, :role => Role.owner)
+  end
+
+  def add_stocker(user)
+    Job.create!(:restaurant => self, :user => user, :role => Role.stocker)
+  end
+
+  def add_cook(user)
+    Job.create!(:restaurant => self, :user => user, :role => Role.cook)
   end
 
   def approved?
