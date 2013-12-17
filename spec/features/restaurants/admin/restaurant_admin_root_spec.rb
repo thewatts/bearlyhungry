0e8f88ce26088425d8fa8657e233783a6823ed82
Nathaniel Watts <reg@nathanielwatts.com>
2013-12-16 18:20:06 -0700

    add headers for rest stats

--- spec/features/restaurants/admin/restaurant_admin_root_spec.rb
+++ spec/features/restaurants/admin/restaurant_admin_root_spec.rb
@@ -9,12 +9,19 @@ describe "Restaurant Admin Goes to Restaurant Admin Root" do
 
   before do
     FactoryGirl.create(:role, name: "Owner")
+    mcdonalds.add_owner(user)
     sign_in(user)
   end
 
   it "shows the dashboard for a specific restaurant" do
-    mcdonalds.add_owner(user)
     visit restaurant_admin_root_path(mcdonalds.slug)
     expect(page).to have_content "#{mcdonalds.name} Dashboard"
+  end
+
+  it "shows the stats for the restaurant" do
+    visit restaurant_admin_root_path(mcdonalds.slug)
+    expect(page).to have_content "Today's Orders"
+    expect(page).to have_content "Today's Sales"
+    expect(page).to have_content "New Customers"
   end
 end
