b759e5327c6c503eb187e52b0f2df6c391a83ea5
Nathaniel Watts <reg@nathanielwatts.com>
2013-12-18 16:40:00 -0700

    fix OrderItemControllerSpec

--- spec/controllers/order_items_controller_spec.rb
+++ spec/controllers/order_items_controller_spec.rb
@@ -2,78 +2,77 @@ require 'spec_helper'
 
 describe OrderItemsController do
 
+  before do
+    request.env["HTTP_REFERER"] = root_path
+    @mcdonalds  = FactoryGirl.create(:restaurant, :name => "McDonalds",
+                                                  :slug => "mcdonalds")
+    @burgerking = FactoryGirl.create(:restaurant, :name => "BK",
+                                                  :slug => "bk")
+
+    @mcdonalds_order  = FactoryGirl.create(:order, :restaurant => @mcdonalds)
+    @burgerking_order = FactoryGirl.create(:order, :restaurant => @burgerking)
+
+    @big_mac = FactoryGirl.create(:item, :title => "Big Mac",
+                                         :restaurant => @mcdonalds)
+    @order_key = "#{@mcdonalds.slug}_order_id".to_sym
+  end
+
   describe "#create" do
-    before do
-      @restaurant = FactoryGirl.create(:restaurant)
-      @restaurant2 = FactoryGirl.create(:restaurant, :name => "BK", 
-                                                     :slug => "bk")
-      @order1 = FactoryGirl.create(:order, :restaurant => @restaurant)
-      @order2 = FactoryGirl.create(:order, :restaurant => @restaurant2)
-      @item = FactoryGirl.create(:item, :restaurant => @restaurant)
-      request.env["HTTP_REFERER"] = root_path
-      @order_key = "#{@restaurant.slug}_order_id".to_sym
-    end
 
     context "order status defaults to pending" do
       it "adds an order item" do
-        session[@order_key] = @order1.id
-        expect(@order1.order_items.count).to eq(0)
+        session[@order_key] = @mcdonalds.id
+        expect(@mcdonalds_order.order_items.count).to eq(0)
 
-        post :create, order_item: {item_id: @item.id, quantity: 5},
-                      slug: @restaurant.slug
-        expect(@order1.order_items.count).to eq(1)
-      end
-    end
-    
-    context "an invalid order" do
-      it "doesn't add an order item" do
-        session[@order_key] = @order2.id
-        expect(@order1.order_items.count).to eq(0)
-
-        post :create, order_item: { item_id: @item.id, quantity: 5 },
-                      slug: @restaurant.slug
-        expect(@order1.order_items.count).to eq(0)
+        post :create, order_item: {item_id: @big_mac.id, quantity: 5},
+                      slug: @mcdonalds.slug
+        expect(@mcdonalds_order.order_items.count).to eq(1)
       end
     end
 
     context "the restaurant doesn't have that item" do
-      it "shouldn't update the order's order_item count"
+      it "shouldn't update the order's order_item count" do
+        session["#{@burgerking.slug}_order_id"] = @burgerking.id
+        expect(@burgerking_order.order_items.count).to eq(0)
+
+        post :create, order_item: { item_id: @big_mac.id, quantity: 5 },
+                      slug: @burgerking.slug
+        expect(@burgerking_order.order_items.count).to eq(0)
+      end
     end
   end
 
   describe "#destroy" do
 
-    before do
-      @restaurant = FactoryGirl.create(:restaurant)
-      FactoryGirl.create(:item, :restaurant => @restaurant)
-      @order1 = FactoryGirl.create(:order, :restaurant => @restaurant)
-      @order2 = FactoryGirl.create(:order)
-      @order_item1 = FactoryGirl.create(:order_item, order_id: @order1.id)
-      @order_key = "#{@restaurant.slug}_order_id".to_sym
-    end
-
     context "a valid order" do
-      it "destroys an order item" do
-        session[@order_key] = @order1.id
-        expect(Order.count).to eq(2)
-        expect(OrderItem.count).to eq(1)
-        expect(@order1.order_items.count).to eq(1)
 
-        delete :destroy, id: @order_item1.id,
-                         slug: @restaurant.slug
+      it "destroys an order item" do
+        @order_key = "#{@mcdonalds.slug}_order_id".to_sym
+        @mcdonalds_order.add_item(@big_mac, 5)
+
+        session[@order_key] = @mcdonalds_order.id
+        expect(@mcdonalds_order.order_items.count).to eq(1)
+
+        delete :destroy, id: @mcdonalds_order.order_items.first.id,
+                         slug: @mcdonalds.slug
         expect(OrderItem.count).to eq(0)
-        expect(@order1.order_items.count).to eq(0)
-        expect(session[@order_key]).to eq(1)
+        expect(@mcdonalds_order.order_items.count).to eq(0)
+        expect(session[@order_key]).to eq(@mcdonalds_order.id)
       end
     end
 
     context "for an invalid order" do
+      let!(:order_item) { FactoryGirl.create(:order_item, order: @burkerking_order) }
       it "cannot destroy an order item" do
-        session[@order_key] = 2
-        delete :destroy, id: @order_item1.id,
-                         slug: @restaurant.slug
-        expect(@order1.order_items.count).to eq(1)
-        expect(session[@order_key]).to eq(2)
+        @mcdonalds_order.add_item(@big_mac, 5)
+        expect(@mcdonalds_order.order_items.count).to eq(1)
+
+        @order_key = "#{@mcdonalds.slug}_order_id".to_sym
+        session[@order_key] = @mcdonalds_order.id
+        delete :destroy, id: order_item.id,
+                         slug: @mcdonalds.slug
+        expect(@mcdonalds_order.order_items.count).to eq(1)
+        expect(session[@order_key]).to eq(@mcdonalds_order.id)
       end
     end
 
