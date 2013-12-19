884e0329c546be49850b98e674afe6912c114bbc
Katrina Engelsted <katrina@engelsted.co>
2013-12-19 07:11:44 -0700

    still cant get json to render... mother efer

--- app/controllers/home_page_controller.rb
+++ app/controllers/home_page_controller.rb
@@ -6,20 +6,16 @@ class HomePageController < ApplicationController
   end
 
   def map_data
-    cities = City.all
-    data = cities.map { |city|
-      {
-        :name => city.city,
-        :count => city.restaurants.count,
-        :url => "http://google.com"
-      }
-    }
-    respond_to do |format|
-      format.json do
-        render json: {
-          cities: data
-        }.to_json
-      end
+    @cities = City.all
+    # data = cities.map { |city|
+    #   {
+    #     :name => city.city,
+    #     :count => city.restaurants.count,
+    #     :url => "http://google.com"
+    #   }
+    # }
+    respond_to do |format|    
+      format.json { render :json => @cities.to_json(:only => [:cities, :coordinates]) }
     end
   end
 end
