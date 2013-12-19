a94f6731c19094f8d2ad7a51f9d716ec12eacda9
Nathaniel Watts <reg@nathanielwatts.com>
2013-12-19 15:10:34 -0700

    remove fonts from assets

--- config/application.rb
+++ config/application.rb
@@ -19,10 +19,8 @@ module Wtpho
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    #config.assets.paths << "#{Rails.root}/app/assets/fonts"
+    config.assets.paths << "#{Rails.root}/app/assets/fonts"
     config.i18n.enforce_available_locales = false
     # add app/assets/fonts to the asset path
-    config.assets.paths << Rails.root.join("app", "assets", "fonts")
-    
   end
 end
