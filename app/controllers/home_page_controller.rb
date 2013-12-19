c89d9450bf201da0a96efaf61c832322033d655e
Nathaniel Watts <reg@nathanielwatts.com>
2013-12-19 14:35:49 -0700

    make map work! with JSONgit add .git add .!

--- app/controllers/home_page_controller.rb
+++ app/controllers/home_page_controller.rb
@@ -3,9 +3,36 @@ class HomePageController < ApplicationController
 
   def index
     @restaurants = Restaurant.limit(4)
+    @data = city_map_data
   end
 
   def map_data
+    respond_to do |format|
+      format.json do
+        render json: {
+          cities: city_map_data
+        }.to_json
+      end
+    end
+  end
 
+  def city_map_data
+    cities = City.all
+    cities.map do |city|
+      if city.restaurants.count > 0
+        {
+          :type => "Feature",
+          :properties => {
+            :name  => city.city,
+            :count => city.restaurants.count,
+            :slug  => "http://google.com"
+          },
+          :geometry => {
+            :type => "Point",
+            :coordinates => city.coordinates
+          }
+        }
+      end
+    end.compact
   end
 end
