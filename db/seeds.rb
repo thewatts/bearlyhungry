d7269bb6e01c2cd77d3923e92b4f73bc37278542
Nathaniel Watts <reg@nathanielwatts.com>
2013-12-20 10:27:28 -0700

    add coords to eed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
 require 'benchmark'
 require 'csv'
 require 'bcrypt'
-require 'pry'
 
 unencrypted_password = "password"
 encrypted_password   = BCrypt::Password.create(unencrypted_password)
@@ -11,6 +10,9 @@ time = Benchmark.measure do
 ##############################################################################
 # Counts for Models
 ##############################################################################
+#@user_num       = 100_000
+#@restaurant_num = 1000
+
 @user_num       = 10
 @restaurant_num = 10
 
@@ -20,41 +22,41 @@ time = Benchmark.measure do
 #
 puts "Creating Locations"
 #
-albuquerque  = City.create!(city: "Albuquerque")
-anchorage    = City.create!(city: "Anchorage")
-atlanta      = City.create!(city: "Atlanta")
-austin       = City.create!(city: "Austin")
-boston       = City.create!(city: "Boston")
+albuquerque  = City.create!(city: "Albuquerque", coordinates: '[-106.64864,35.08418]')
+anchorage    = City.create!(city: "Anchorage", coordinates: '[-149.85838,61.21756]' )
+atlanta      = City.create!(city: "Atlanta", coordinates: '[-84.39111,33.74832]')
+austin       = City.create!(city: "Austin", coordinates: '[-97.74298,30.2676]')
+boston       = City.create!(city: "Boston", coordinates: '[-71.05675,42.35866]')
 
-boulder      = City.create!(city: "Boulder")
-burlington   = City.create!(city: "Burlington")
-charlotte    = City.create!(city: "Charlotte")
-chicago      = City.create!(city: "Chicago")
-cincinnati   = City.create!(city: "Cincinatti")
+boulder      = City.create!(city: "Boulder", coordinates: '[-105.27924,40.01574]')
+burlington   = City.create!(city: "Burlington", coordinates: '[-73.21323,44.47592]')
+charlotte    = City.create!(city: "Charlotte", coordinates: '[-80.83796,35.22287]')
+chicago      = City.create!(city: "Chicago", coordinates: '[-79.99748,40.43834]')
+cincinnati   = City.create!(city: "Cincinatti", coordinates: '[-84.50413,39.10713]')
 
-chicago      = City.create!(city: "Chicago")
-dallas       = City.create!(city: "Dallas")
-dc           = City.create!(city: "Washington, D.C.")
-detroit      = City.create!(city: "Detriot")
-denver       = City.create!(city: "Denver")
+chicago      = City.create!(city: "Chicago", coordinates: '[-87.63245,41.88425]')
+dallas       = City.create!(city: "Dallas", coordinates: '[-96.7954,32.77816]')
+dc           = City.create!(city: "Washington, D.C.", coordinates: '[-77.03196,38.89037]')
+detroit      = City.create!(city: "Detriot", coordinates: '[-83.048,42.33168]')
+denver       = City.create!(city: "Denver", coordinates: '[-104.9923,39.74001]')
 
-honolulu     = City.create!(city: "Honolulu")
-la           = City.create!(city: "Los Angeles")
-little_rock  = City.create!(city: "Little Rock")
-madison      = City.create!(city: "Madison")
-miami        = City.create!(city: "Miami")
+honolulu     = City.create!(city: "Honolulu", coordinates: '[-157.85775,21.30486]')
+la           = City.create!(city: "Los Angeles", coordinates: '[-118.24532,34.05349]')
+little_rock  = City.create!(city: "Little Rock", coordinates: '[-92.27449,34.74866]')
+madison      = City.create!(city: "Madison", coordinates: '[-89.38669,43.07295]')
+miami        = City.create!(city: "Miami", coordinates: '[-80.19773,25.77481]')
 
-minneapolis  = City.create!(city: "Minneapolis")
-nyc          = City.create!(city: "New York City")
-philadelphia = City.create!(city: "Philadelphia")
-pittsburgh   = City.create!(city: "Pittsburgh")
-portland     = City.create!(city: "Portland")
+minneapolis  = City.create!(city: "Minneapolis", coordinates: '[-93.26493,44.97904]')
+nyc          = City.create!(city: "New York City", coordinates: '[-74.00712,40.71455]')
+philadelphia = City.create!(city: "Philadelphia", coordinates: '[-75.16245,39.95228]')
+pittsburgh   = City.create!(city: "Pittsburgh", coordinates: '[-79.99748,40.43834]')
+portland     = City.create!(city: "Portland", coordinates: '[-122.67563,45.5118]')
 
-san_diego    = City.create!(city: "San Diego")
-santa_fe     = City.create!(city: "Santa Fe")
-seattle      = City.create!(city: "Seattle")
-sanfran      = City.create!(city: "San Francisco")
-tucson       = City.create!(city: "Tucson")
+san_diego    = City.create!(city: "San Diego", coordinates: '[-117.16172,32.71569]')
+santa_fe     = City.create!(city: "Santa Fe", coordinates: '[-105.93796,35.69168]')
+seattle      = City.create!(city: "Seattle", coordinates: '[-122.32945,47.60358]')
+sanfran      = City.create!(city: "San Francisco", coordinates: '[-122.41964,37.77713]')
+tucson       = City.create!(city: "Tucson", coordinates: '[-110.96976,32.22155]')
 
 cities = [
   albuquerque, anchorage, atlanta, austin, boston, boulder, burlington,
@@ -286,5 +288,29 @@ restaurants.each { |r| clone_restaurant(r, cities, @restaurant_num) }
 #images = File.open "./app/assets/images"
 end
 puts "Time to seed:"
+puts time
+puts time
+puts time
+puts time
+puts time
+puts time
+puts time
+puts time
+puts time
+puts time
+puts time
+puts time
+puts time
+puts time
+puts time
+puts time
+puts time
+puts time
+puts time
+puts time
+puts time
+puts time
+puts time
+puts time
 puts time
 
